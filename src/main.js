19afc32d754b666eba684f03b8a95a55f14ecbc9
Emmanuel Chi <iheanyie5@gmail.com>
2026-05-26 20:15:48 +0100

    Fix styles and update build setup again

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { useAuthStore } from '@/stores/authStore'
 
-
 import App from './App.vue'
 import router from './router'
-import '/src/style.css'
-
+import './style.css'
 const app = createApp(App)
 
 const pinia = createPinia()
